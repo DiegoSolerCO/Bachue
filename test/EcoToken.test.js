2fc7afc2897e5d102bafc54a967bfe89152ba4f6
DiegoSolerCO <127995751+DiegoSolerCO@users.noreply.github.com>
2023-03-17 16:15:58 -0500

    Update EcoToken.test.js

--- test/EcoToken.test.js
+++ test/EcoToken.test.js
@@ -1,1 +1,21 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
+describe("EcoToken", function () {
+  let EcoToken, ecoToken, owner, addr1, addr2;
+
+  beforeEach(async () => {
+    EcoToken = await ethers.getContractFactory("EcoToken");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    ecoToken = await EcoToken.deploy();
+    await ecoToken.deployed();
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await ecoToken.platformOwner()).to.equal(owner.address);
+    });
+  });
+
+  // Add more tests for the EcoToken contract here
+});
