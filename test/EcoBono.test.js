9d264ff33074d9f7051c0a0ec6ea5086bbbb05b6
DiegoSolerCO <127995751+DiegoSolerCO@users.noreply.github.com>
2023-03-17 16:15:05 -0500

    Update EcoBono.test.js

--- test/EcoBono.test.js
+++ test/EcoBono.test.js
@@ -1,1 +1,21 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
+describe("EcoBono", function () {
+  let EcoBono, ecoBono, owner, addr1, addr2;
+
+  beforeEach(async () => {
+    EcoBono = await ethers.getContractFactory("EcoBono");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    ecoBono = await EcoBono.deploy();
+    await ecoBono.deployed();
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await ecoBono.platformOwner()).to.equal(owner.address);
+    });
+  });
+
+  // Add more tests for the EcoBono contract here
+});
