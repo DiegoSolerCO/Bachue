d74bcd25282c9587896a58970fed0d119070a4d2
DiegoSolerCO <127995751+DiegoSolerCO@users.noreply.github.com>
2023-03-18 16:18:11 -0500

    Create CreateProducto.js

---
+++ src/components/CreateProducto.js
@@ -0,0 +1,16 @@
+// CreateProducto.js
+import React, { Component } from "react";
+
+class CreateProducto extends Component {
+  // Aquí puedes implementar la lógica para crear un nuevo Producto (Chiminigagua NFT)
+  render() {
+    return (
+      <div>
+        <h2>Crear Producto</h2>
+        {/* Aquí puedes agregar un formulario para crear nuevos Productos */}
+      </div>
+    );
+  }
+}
+
+export default CreateProducto;
