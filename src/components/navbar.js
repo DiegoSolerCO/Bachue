54900c2fcadcdc4f10f9de73b8c072f618f731a2
DiegoSolerCO <127995751+DiegoSolerCO@users.noreply.github.com>
2023-03-18 16:16:43 -0500

    Create navbar.js

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">Bachue NFT Marketplace</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
