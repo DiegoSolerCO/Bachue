03dc811fb4a330510a4c3a91e6a7783c40e0815a
DiegoSolerCO <127995751+DiegoSolerCO@users.noreply.github.com>
2023-03-18 16:17:18 -0500

    Create Createbono.js

---
+++ src/components/Createbono.js
@@ -0,0 +1,16 @@
+// CreateBono.js
+import React, { Component } from "react";
+
+class CreateBono extends Component {
+  // Aquí puedes implementar la lógica para crear un nuevo Bono (Chibchacum NFT)
+  render() {
+    return (
+      <div>
+        <h2>Crear Bono</h2>
+        {/* Aquí puedes agregar un formulario para crear nuevos Bonos */}
+      </div>
+    );
+  }
+}
+
+export default CreateBono;
